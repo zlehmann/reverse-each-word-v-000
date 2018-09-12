4ce46cd50025e2a5fb1cf0dca2e3b8c0aed47cb7
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-12 01:48:19 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.chars
   output = []
   array.each do |letter|
-    output.push(letter)
+    output.shift(letter)
   end
   return output.join("")
 end
