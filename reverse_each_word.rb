a1291382dc303d594b2308dc23dc9296853cf68f
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-12 01:41:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 def reverse_each_word(sentence)
   output = []
-  sentence.each_char { |chr| output.push(chr) }
-  return output
+  sentence.each do |letter|
+    output << letter
+  return output.split("")
 end
